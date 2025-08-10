449344ff2d56abdf47e3fcc566687874407bbe62
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2025-08-10 18:57:49 +0600

    add 5

---
+++ Task-5.js
@@ -0,0 +1,11 @@
+const n = 122;
+const res = odd_even(n);
+
+ function odd_even(n){
+    if(n%2 === 0){
+        return console.log('Even')
+    }
+    else{
+         return console.log('Odd')
+    }
+ }
